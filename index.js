774367e9fae88a430c3ea5a3e01193fddde8708d
Max <ik@maximsnoep.com>
2021-07-18 22:05:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('i ❤️ kechje')
+  res.send('i ❤️ maxim')
 })
 
 const port = process.env.PORT || 19999;
