ff2c66758e27acd8c72239c4dbd8b75736919c94
Maxim Snoep <m.snoep@student.tue.nl>
2021-07-18 18:48:08 +0200

    hello world express app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('i ❤️ kechje')
+})
+
+const port = process.env.PORT || 19999;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${port}`);
+});
